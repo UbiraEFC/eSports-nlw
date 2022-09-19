c3d46354f9690bb985da4703ce73123885d39385
UbiraEFC <ubirata.efc@gmail.com>
2022-09-19 14:47:54 -0300

    Final Class

---
+++ Server/src/utils/convert-hour-string-to-minute-number.ts
@@ -0,0 +1,7 @@
+export function convertHourStringToMinuteNumber(hourString: string) {
+	const [hours, minutes] = hourString.split(':').map(Number);
+
+	const minutesAmount = (hours * 60) + minutes;
+
+	return minutesAmount;
+}
